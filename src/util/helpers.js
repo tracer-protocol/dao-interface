3f3db389a1c1a93db20dabc5f4ccc0dbdace4fac
alecdwm <alec@owls.io>
2021-05-20 15:53:19 +1000

    added toWei helper fn

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,8 +1,10 @@
 import BigNumber from 'bignumber.js'
-import Web3 from 'web3';
+import Web3 from 'web3'
 const web3 = new Web3()
 
-export const truncateString = (addr, start=6, end=4) => addr ? `${addr.substring(0, start)}...${addr.substring(addr.length - end)}` : null
-export const numberToMaxDb = (value, dp=2) => +parseFloat(value).toFixed( dp )
-export const fromWei = (amount=0, unit='ether') => web3.utils.fromWei(amount.toString(), unit)
-export const maxApproval = new BigNumber(2).pow(256).minus(1);
+export const truncateString = (addr, start = 6, end = 4) =>
+	addr ? `${addr.substring(0, start)}...${addr.substring(addr.length - end)}` : null
+export const numberToMaxDb = (value, dp = 2) => +parseFloat(value).toFixed(dp)
+export const toWei = (amount = 0, unit = 'ether') => web3.utils.toWei(amount, unit)
+export const fromWei = (amount = 0, unit = 'ether') => web3.utils.fromWei(amount.toString(), unit)
+export const maxApproval = new BigNumber(2).pow(256).minus(1)
